db09412da7f1d24b91e5ea6efa048da19632cb7f
Jean <jean13franca@gmail.com>
2022-11-27 19:09:12 -0300

    feat/ added back to api.services

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,10 +1,13 @@
 import axios from 'axios'
+import { getFromLocalStorage } from '../utils/localStorage';
 
 const { REACT_APP_BASE_URL } = process.env
 
-const BASE_URL = REACT_APP_BASE_URL ? REACT_APP_BASE_URL : 'http://localhost:4000/'
+const BASE_URL = REACT_APP_BASE_URL ? REACT_APP_BASE_URL : 'http://localhost:4001/'
 
-function config(token) {
+const token=getFromLocalStorage()
+
+function config() {
     return {
         headers: { Authorization: `Bearer ${token}` },
     };
@@ -16,8 +19,20 @@ function postSignUp(body) {
 function postSignIn(body) {
     return axios.post(`${BASE_URL}auth/sign-in`, body)
 }
+function postCompany(body) {
+    return axios.post(`${BASE_URL}company`, body, config())
+}
+function getMyCompanies() {
+    return axios.get(`${BASE_URL}company`, config())
+}
+function getOneCompany(body) {
+    return axios.post(`${BASE_URL}company/local-responsible`, body, config())
+}
 
 export {
     postSignUp,
     postSignIn,
+    postCompany,
+    getMyCompanies,
+    getOneCompany
 }
